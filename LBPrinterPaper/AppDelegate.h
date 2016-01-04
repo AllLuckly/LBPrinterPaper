939af38442f122d5f152a4a04bbb9a0d98ed46be
yscf <yscf@yscfdemac-mini.local>
2016-01-04 15:50:45 +0800

    Initial Commit

---
+++ LBPrinterPaper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LBPrinterPaper
+//
+//  Created by yscf on 16/1/4.
+//  Copyright © 2016年 Bison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LBPrinterPaper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LBPrinterPaper
+//
+//  Created by yscf on 16/1/4.
+//  Copyright © 2016年 Bison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
